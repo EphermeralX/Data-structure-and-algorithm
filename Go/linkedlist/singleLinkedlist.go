1cf503542bab6ffe1395e85f3549766addef0ac7
EphermeralX <1048933185@qq.com>
2021-06-30 21:47:57 +0800

    go linkedlist

---
+++ Go/linkedlist/singleLinkedlist.go
@@ -0,0 +1,123 @@
+package linkedlist
+
+import "fmt"
+
+type ListNode struct {
+	next  *ListNode
+	value interface{}
+}
+
+type LinkedList struct {
+	head   *ListNode
+	length uint
+}
+
+func NewListNode(v interface{}) *ListNode {
+	return &ListNode{nil, v}
+}
+
+func (this *ListNode) GetNext() *ListNode {
+	return this.next
+}
+
+func (this *ListNode) GetValue() interface{} {
+	return this.value
+}
+
+func NewLinkedList() *LinkedList {
+	return &LinkedList{NewListNode(0), 0}
+}
+
+func (this *LinkedList) InsertAfter(p *ListNode, v interface{}) bool {
+	if nil == p {
+		return false
+	}
+	newNode := NewListNode(v)
+	oldNext := p.next
+	p.next = newNode
+	newNode.next = oldNext
+	this.length++
+	return true
+}
+
+func (this *LinkedList) InsertBefore(p *ListNode, v interface{}) bool {
+	if nil == p || p == this.head {
+		return false
+	}
+	cur := this.head.next
+	pre := this.head
+	for nil != cur {
+		if cur == p {
+			break
+		}
+		pre = cur
+		cur = cur.next
+	}
+	if nil == cur {
+		return false
+	}
+	newNode := NewListNode(v)
+	pre.next = newNode
+	newNode.next = cur
+	this.length++
+	return true
+}
+
+func (this *LinkedList) InsertToHead(v interface{}) bool {
+	return this.InsertAfter(this.head, v)
+}
+
+func (this *LinkedList) InsertToTail(v interface{}) bool {
+	cur := this.head
+	for nil != cur.next {
+		cur = cur.next
+	}
+	return this.InsertAfter(cur, v)
+}
+
+func (this *LinkedList) FindByIndex(index uint) *ListNode {
+	if index >= this.length {
+		return nil
+	}
+	cur := this.head.next
+	var i uint = 0
+	for ; i < index; i++ {
+		cur = cur.next
+	}
+	return cur
+}
+
+func (this *LinkedList) DeleteNode(p *ListNode) bool {
+	if nil == p {
+		return false
+	}
+	cur := this.head.next
+	pre := this.head
+	for nil != cur {
+		if cur == p {
+			break
+		}
+		pre = cur
+		cur = cur.next
+	}
+	if nil == cur {
+		return false
+	}
+	pre.next = p.next
+	p = nil
+	this.length--
+	return true
+}
+
+func (this *LinkedList) Print() {
+	cur := this.head.next
+	format := ""
+	for nil != cur {
+		format += fmt.Sprintf("%+v", cur.GetValue())
+		cur = cur.next
+		if nil != cur {
+			format += "->"
+		}
+	}
+	fmt.Println(format)
+}
