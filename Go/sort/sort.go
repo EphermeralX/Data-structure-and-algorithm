490d490d996e95343daeeb5a6180f906bf83daab
EphermeralX <1048933185@qq.com>
2021-07-14 23:35:48 +0800

    go sort

---
+++ Go/sort/sort.go
@@ -0,0 +1,54 @@
+package sort
+
+func BubbleSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 0; i < n; i++ {
+		flag := false
+		for j := 0; j < n-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+				flag = true
+			}
+		}
+
+		if !flag {
+			break
+		}
+	}
+}
+
+func InsertionSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 1; i < n; i++ {
+		value := a[i]
+		j := i - 1
+		for ; j >= 0; j-- {
+			if a[j] > value {
+				a[j+1] = a[j]
+			} else {
+				break
+			}
+		}
+		a[j+1] = value
+	}
+}
+
+func SelectionSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 0; i < n; i++ {
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if a[j] < a[minIndex] {
+				minIndex = j
+			}
+		}
+		a[i], a[minIndex] = a[minIndex], a[i]
+
+	}
+}
